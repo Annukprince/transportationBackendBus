31acc3668df738045616d868f45f1674d2ef1af8
Annukprince <annukprince@gmail.com>
2024-09-19 17:26:41 +0530

    first commit

---
+++ src/main/java/com/example/busmanagement/TransportationApplication.java
@@ -0,0 +1,15 @@
+package com.example.busmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class TransportationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransportationApplication.class, args);
+	}
+
+}
